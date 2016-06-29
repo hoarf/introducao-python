d3d00e2a0fd7b020370efa4a700c43bbd39d0f54
hoarf <alan.ficagna@gmail.com>
2016-06-29 19:44:22 -0300

    Add some more lectures

--- code.py
+++ code.py
@@ -7,20 +7,38 @@ print(10>9)
 print(15<12)
 print(15<=15)
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+# Uso de variáveis e memória.
 
-print("Let's talk about %s." % my_name)
-print("He's %d inches tall." % my_height)
-print("He's %d pounds heavy." % my_weight)
-print("Actually that's not too heavy.")
-print("He's got %s eyes and %s hair." % (my_eyes, my_hair))
-print("His teeth are usually %s depending on the coffee." % my_teeth)
+my_name = "Alan"
+my_age = 31
+my_height = 1.79 # metros
+my_weight = 80 # kilos
+my_eyes = "Castanhos"
+my_hair = "Escuros"
 
-# this line is tricky, try to get it exactly right
-print("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
+print()
+print(my_name)
+print(my_height)
+print(my_hair)
+print(my_weight)
+
+# Como lidar com strings.
+# %d : use com digitos
+# %s : use com strings
+# %r : use quando você quer só uma _representação_ do valor
+print("Vamos falar sobre %s" % my_name)
+print("Eu tenho %d metros de altura" % my_height)
+print("Ele pesa %d kilos" % my_weight)
+print("Pra falar a verdade isso nem é muito pesado")
+print("Ele tem os olhos %s e os cabelos %s" % (my_eyes, my_hair))
+
+# Essa linha é complicada
+print("Se eu somar %d, %d, e %d isso da %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
+
+# Fazendo perguntas ao usuário.
+user_age = input("Quantos anos você tem ")
+user_weight = input("Quanto você pesa ")
+user_height = input("Quanto você mede ")
+print("Então você tem %r anos, pesa %r kilos e mede %r metros!" % (user_age, user_weight, user_height))
+
+* [ ] Exercício: Fazer um programa que pergunte o peso e a altura do usuário e devolva o Índice de Massa Corporál (SE VIREM)
